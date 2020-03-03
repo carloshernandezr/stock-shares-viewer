0241d15790f3c8424388e137fc87ff2e842b99d4
Carlos Hernandez <carlos@devcodify.com>
2020-03-02 21:02:19 -0700

    add .travis.yml and fix some error

--- models/group.js
+++ models/group.js
@@ -1,22 +1,22 @@
 module.exports = function (sequelize, DataTypes) {
-    let Group = sequelize.define("Group", {
-        groupName: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1, 50]
-            }
-        }
-    });
+  const Group = sequelize.define('Group', {
+    groupName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 50]
+      }
+    }
+  })
 
-    // ADD ASSOCIATIONS HERE
-    Group.associate = function(models) {
-        Group.hasMany(models.Watchlist, {
-            onDelete: "cascade"
-        });
-        Group.hasMany(models.Portfolio, {
-            onDelete: "cascade"
-        });
-    };
-    return Group;
+  // ADD ASSOCIATIONS HERE
+  Group.associate = function (models) {
+    Group.hasMany(models.Watchlist, {
+      onDelete: 'cascade'
+    })
+    Group.hasMany(models.Portfolio, {
+      onDelete: 'cascade'
+    })
+  }
+  return Group
 }
--- models/portfolio.js
+++ models/portfolio.js
@@ -1,51 +1,51 @@
 module.exports = function (sequelize, DataTypes) {
-    let Portfolio = sequelize.define("Portfolio", {
-        date: DataTypes.DATE,
-        ticker: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1, 10]
-            }
-        },
-        shares: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            validate: {
-                min: 1
-            }
-        },
-        purchasePrice: {
-            type: DataTypes.DECIMAL(10, 2),
-            allowNull: false,
-            validate: {
-                min: 0
-            }
-        },
-        sellDate: DataTypes.DATE,
-        sellPrice: {
-            type: DataTypes.DECIMAL(10, 2),
-            allowNull: false,
-            validate: {
-                min: 0
-            }
-        },
-        soldShares: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            validate: {
-                min: 1
-            }
-        },
-    });
+  const Portfolio = sequelize.define('Portfolio', {
+    date: DataTypes.DATE,
+    ticker: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 10]
+      }
+    },
+    shares: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        min: 1
+      }
+    },
+    purchasePrice: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      validate: {
+        min: 0
+      }
+    },
+    sellDate: DataTypes.DATE,
+    sellPrice: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      validate: {
+        min: 0
+      }
+    },
+    soldShares: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        min: 1
+      }
+    }
+  })
 
-    // ADD ASSOCIATIONS HERE
-    Portfolio.associate = function (models) {
-        Portfolio.belongsTo(models.Group, {
-            foreignKey: {
-                allowNull: false
-            }
-        })
-    };
-    return Portfolio
+  // ADD ASSOCIATIONS HERE
+  Portfolio.associate = function (models) {
+    Portfolio.belongsTo(models.Group, {
+      foreignKey: {
+        allowNull: false
+      }
+    })
+  }
+  return Portfolio
 }
