5280cb37000df3f888698c5e9141cc48afabd6d2
Arnette110 <christopher.ryan.ellis@gmail.com>
2020-03-07 14:33:53 -0700

    added logic for rendering chart

--- public/js/watchlist.js
+++ public/js/watchlist.js
@@ -56,15 +56,13 @@ $(document).ready(function () {
     // Send the PUT request.
     $.ajax('/api/watchlist/search/' + ticker, {
       type: 'GET'
-    }).then(
-      function (response) {
-        console.log('API response', response)
-        createMessage(response)
-        createChart(response)
-        // Reload the page to get the updated list
-        // location.reload();
-      }
-    )
+    }).then(function (response) {
+      console.log('API response', response)
+      createMessage(response)
+      createChart(response)
+      // Reload the page to get the updated list
+      // location.reload();
+    })
   })
   function createMessage (data) {
     const newMessage = $(`<article class="message">
@@ -84,7 +82,7 @@ $(document).ready(function () {
     <li>Market Cap: ${data[0].marketCap} </li>
     <li>YTD%: ${data[0].ytdChange} </li>
     </ul>
-    CANVAS CHART GOES HERE
+    <div id="chartContainer" style="height: 370px; width: 100%;"></div>
 
     <p><a class="button is-info" id="newListBtn">
                     Add to watchlist
@@ -94,7 +92,73 @@ $(document).ready(function () {
     $('#watchlistContent').append(newMessage)
   }
   function createChart (data) {
-    console.log(data[1])
+    // console.log(data[1])
+    data[1].forEach(el => {
+      console.log(el.x)
+      el.x = new Date(el.x);
+    });
+    for (let i = 0; i < data[1].length; i++) {
+      // console.log(data[1][i]);
+      var chart = new CanvasJS.Chart('chartContainer', {
+        animationEnabled: true,
+        theme: 'light2', // "light1", "light2", "dark1", "dark2"
+        exportEnabled: true,
+        title: {
+          text: data[0].company
+        },
+        axisX: {
+          interval: 1,
+          valueFormatString: 'MM DD YYYY'
+          // text: data[1][i].x
+        },
+        axisY: {
+          includeZero: false,
+          prefix: '$',
+          title: 'Price'
+        },
+        toolTip: {
+          content:
+            'Date: {x}<br /><strong>Price:</strong><br />Open: {y[0]}, Close: {y[3]}<br />High: {y[1]}, Low: {y[2]}'
+        },
+        data: [
+          {
+            type: 'candlestick',
+            yValueFormatString: '$##0.00',
+            dataPoints: data[1],
+            xValueType: "dateTime"
+          }
+        ]
+      })
+
+      chart.render()
+    }
+
+    // $.get(
+    //   'https://canvasjs.com/data/gallery/javascript/netflix-stock-price.csv',
+    //   getDataPointsFromCSV
+    // )
+
+    // function getDataPointsFromCSV (csv) {
+    //   var csvLines = (points = [])
+    //   csvLines = csv.split(/[\r?\n|\r|\n]+/)
+    //   for (var i = 0; i < csvLines.length; i++) {
+    //     if (csvLines[i].length > 0) {
+    //       points = csvLines[i].split(',')
+    //       dataPoints.push({
+    //         x: new Date(
+    //           parseInt(points[0].split('-')[0]),
+    //           parseInt(points[0].split('-')[1]),
+    //           parseInt(points[0].split('-')[2])
+    //         ),
+    //         y: [
+    //           parseFloat(points[1]),
+    //           parseFloat(points[2]),
+    //           parseFloat(points[3]),
+    //           parseFloat(points[4])
+    //         ]
+    //       })
+    //     }
+    //   }
   }
-  
 })
+// .x&&m.dataPoints[s].x.getTime||"dateTime"===m.xValueType)l=!0;for(s=0;s<m.dataPoints.length;s++){"undefined"===typeof m.dataPoints[s].x&&(m.dataPoints[s].x=s+(a.axisX.logarithmic?1:0));m.dataPoints[s].x.getTime?(l=!0,c=m.dataPoints[s].x.getTime()):c=m.dataPoints[s].x;if((e=m.dataPoints[s].y)&&e.length){g=Math.min.apply(null,e);k=Math.max.apply(null,e);B=!0;for(var p=0;p<e.length;p++)null===e.k&&(B=!1);B&&(q||(h=f),f=c)}c<b.min&&(b.min=c);c>b.max&&
