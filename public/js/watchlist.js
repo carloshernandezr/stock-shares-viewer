56ce43052b8ec1ad39a05d0f2ea846707ee1e88f
Arnette110 <christopher.ryan.ellis@gmail.com>
2020-03-10 19:09:47 -0600

    edits for travis

--- public/js/watchlist.js
+++ public/js/watchlist.js
@@ -203,7 +203,7 @@ $(document).ready(function () {
   <div id="chartContainer" style="height: 300px; width: 100%;"></div>
   </div>
     </div>
-  </article>`);
+  </article>`)
     $('#watchlistContent').empty()
     $('#watchlistContent').append(newMessage)
   }
@@ -215,37 +215,37 @@ $(document).ready(function () {
     })
     for (let i = 0; i < data[1].length; i++) {
       // eslint-disable-next-line no-undef
-      var chart = new CanvasJS.Chart("chartContainer", {
+      var chart = new CanvasJS.Chart('chartContainer', {
         animationEnabled: true,
-        theme: "dark2", // "light1", "light2", "dark1", "dark2"
+        theme: 'dark2', // "light1", "light2", "dark1", "dark2"
         exportEnabled: true,
         title: {
-          text: data[0].company,
+          text: data[0].company
         },
         axisX: {
           intervalType: 'day',
-          valueFormatString: "MM DD YYYY",
+          valueFormatString: 'MM DD YYYY'
         },
         axisY: {
           includeZero: false,
-          prefix: "$",
-          title: "Price",
+          prefix: '$',
+          title: 'Price'
         },
         toolTip: {
           content:
-            "Date: {x}<br /><strong>Price:</strong><br />Open: {y[0]}, Close: {y[3]}<br />High: {y[1]}, Low: {y[2]}",
+            'Date: {x}<br /><strong>Price:</strong><br />Open: {y[0]}, Close: {y[3]}<br />High: {y[1]}, Low: {y[2]}'
         },
         data: [
           {
-            type: "candlestick",
-            yValueFormatString: "$##0.00",
+            type: 'candlestick',
+            yValueFormatString: '$##0.00',
             dataPoints: data[1],
-            xValueType: "dateTime",
-            risingColor: "#66ff33",
-            color: "#ff0000",
-          },
-        ],
-      });
+            xValueType: 'dateTime',
+            risingColor: '#66ff33',
+            color: '#ff0000'
+          }
+        ]
+      })
 
       chart.render()
     }
