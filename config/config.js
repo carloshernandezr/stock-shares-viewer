3ee185adbf4c9b6d642a6ba5ed50bb34461544a3
Erik Huynh <Erik.W.Huynh@gmail.com>
2020-03-11 20:53:22 -0600

    fixed lint

--- config/config.js
+++ config/config.js
@@ -1,26 +1,26 @@
 module.exports = {
   development: {
-    username: "root",
+    username: 'root',
     password: process.env.MYSQL_PASSWORD,
-    database: "watchlist_db",
-    host: "127.0.0.1",
-    dialect: "mysql",
+    database: 'watchlist_db',
+    host: '127.0.0.1',
+    dialect: 'mysql',
     operatorsAliases: false
   },
   test: {
-    username: "root",
+    username: 'root',
     password: null,
-    database: "database_test",
-    host: "127.0.0.1",
-    dialect: "mysql",
+    database: 'database_test',
+    host: '127.0.0.1',
+    dialect: 'mysql',
     operatorsAliases: false
   },
   production: {
-    username: "root",
+    username: 'root',
     password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql",
+    database: 'database_production',
+    host: '127.0.0.1',
+    dialect: 'mysql',
     operatorsAliases: false
   }
 }
